7b0cf2c181bed3ed69907730bd8ef1ee74376e43
Eugeny <sgenya72@gmail.com>
2022-09-28 22:34:10 +0300

    add new file README

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] arrayString = { "Hello", "2", "World", ":-" };
+string[] arrayDouble = new string[arrayString.Length];
+
+void FillingArrayDouble(string[] arrayString, string[] arrayDouble)
+{
+    int count = 0;
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        if (arrayString[i].Length <= 3)
+        {
+            arrayDouble[count] = arrayString[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ,");
+    }
+    Console.WriteLine();
+}
+
+FillingArrayDouble(arrayString, arrayDouble);
+PrintArray(arrayDouble);
+
+
+
+
+
